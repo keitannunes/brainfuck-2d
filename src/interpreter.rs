1f1fda83de27516620cb1d439e598fbb3087812a
keitannunes <keitannunes@gmail.com>
2024-01-12 11:41:47 -0500

    Added CLAs

---
+++ src/interpreter.rs
@@ -0,0 +1,102 @@
+
+use std::collections::HashMap;
+use std::io::{Read, self};
+
+pub fn interpret(content: Vec<u8>, dimension: usize) -> (usize, Vec<u8>) {
+    let size: usize  = dimension*dimension;
+
+    let mut jumps: HashMap<usize, usize> = HashMap::new();
+    let mut jump_stack: Vec<usize> = Vec::new();
+
+    //first pass
+    let mut i: usize = 0;
+    while i < content.len() {
+        match content[i] {
+            b'>' | b'<' | b'+' | b'-' | b',' | b'.' | b'\n' | b' ' | b'\t' => (), //do nothing
+            b'[' => {
+                jump_stack.push(i);
+            }
+            b']' => {
+                match jump_stack.pop() {
+                    Some(x) => {
+                        jumps.insert(x, i);
+                        jumps.insert(i, x);
+                        i += 1;
+                    }
+                    None => {
+                        eprintln!("Error char {i}: unmatched ] bracket");
+                        std::process::exit(1)
+                    }
+                }
+            }
+            _ => { //comment
+                let comment_start: usize = i;
+                while i < content.len() && content[i] != b'\n' { i += 1; }
+                jumps.insert(comment_start, i);
+            }
+        }
+        i += 1
+    }
+    if !jump_stack.is_empty() {
+        eprintln!("Error char {}: unmatched ] bracket ", jump_stack.pop().unwrap_or_else(|| 0));
+        std::process::exit(1);
+    }
+
+    let mut mem: Vec<u8> = vec![0u8; size];
+    let mut ptr: usize = 0;
+    let mut buffer = [0; 1]; //buffer for reading a character in
+    //second pass
+    i = 0;
+    while i < content.len() {
+        match content[i] {
+            b'>' => ptr = (ptr + 1) % size,
+            b'<' => {
+                if ptr == 0 {
+                    ptr = size - 1;
+                } else {
+                    ptr = (ptr - 1) % size;
+                }
+            }
+            b'+' => mem[ptr] = mem[ptr].wrapping_add(1),
+            b'-' => mem[ptr] = mem[ptr].wrapping_sub(1),
+            b',' => {
+                match io::stdin().read_exact(&mut buffer) {
+                    Ok(_) => {
+                        let character = buffer[0];
+                        if character.is_ascii() {
+                            mem[ptr] = character;
+                        } else {
+                            eprintln!("Runtime Error: Non-ASCII character entered.");
+                            std::process::exit(1);
+                        }
+                    }
+                    Err(error) => println!("Error reading input: {}", error),
+                }
+            }
+            b'.' => print!("{}", mem[ptr] as char),
+            b'[' | b']' => {
+                if mem[ptr] != 0 && content[i] == b']' || mem[ptr] == 0 && content[i] == b'[' {
+                    match jumps.get(&i) {
+                        Some(x) => i = *x,
+                        None => {
+                            eprintln!("Internal Error: Jump at {i} not found");
+                            std::process::exit(1);
+                        }
+                    }
+                }
+            }
+            b'\n' | b'\t' | b' ' => (),
+            _ => { //comment
+                match jumps.get(&i) {
+                    Some(x) => i = *x,
+                    None => {
+                        eprintln!("Internal Error: Jump at {i} not found");
+                        std::process::exit(1);
+                    }
+                }
+            }
+        }
+        i += 1;
+    }
+    return (ptr, mem);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,39 @@
+mod interpreter;
 use std::fs;
 use std::path::Path;
-use std::env;
-use std::collections::HashMap;
-use std::io::{self, Read};
+use clap::Parser;
+#[derive(Parser)] // requires `derive` feature
+#[command(author, about, long_about = None)]
+struct Args {
+    #[arg(short = 'd', value_name = "DIMENSION", default_value_t = 8usize, help = "Dimension of memory")]
+    dimension: usize,
 
-const DIMENSION: i32 = 8;
-const SIZE: usize = (DIMENSION * DIMENSION) as usize;
+    #[arg(short = 'v', help = "View memory dump")]
+    view_memory: bool,
+
+    #[arg()]
+    filename: String,
+}
 
 fn main() {
+
     //get file content
-    let args: Vec<String> = env::args().collect();
-    let path = Path::new(&args[1]);
+    let args = Args::parse();
+    let path = Path::new(&args.filename);
     let content: Vec<u8> = match fs::read_to_string(path) {
         Ok(t) => t.into_bytes(),
         Err(e) => {
-            eprintln!("{}",e);
+            eprintln!("{} ",e);
             std::process::exit(1);
         }
     };
-
-
-    let mut jumps: HashMap<usize, usize> = HashMap::new();
-    let mut jump_stack: Vec<usize> = Vec::new();
-
-    //first pass
-    let mut i: usize = 0;
-    while i < content.len() {
-        match content[i] {
-            b'>' | b'<' | b'+' | b'-' | b',' | b'.' | b'\n' | b' ' | b'\t' => (), //do nothing
-            b'[' => {
-                jump_stack.push(i);
-            }
-            b']' => {
-                match jump_stack.pop() {
-                    Some(x) => {
-                        jumps.insert(x, i);
-                        jumps.insert(i, x);
-                        i += 1;
-                    }
-                    None => {
-                        eprintln!("Error char {i}: unmatched ] bracket");
-                        std::process::exit(1)
-                    }
-                }
-            }
-            _ => { //comment
-                let comment_start: usize = i;
-                while i < content.len() && content[i] != b'\n' { i += 1; }
-                jumps.insert(comment_start, i);
-            }
+    let dimension = args.dimension;
+    let (ptr, mem) = interpreter::interpret(content, dimension);
+    if args.view_memory {
+        println!("---\nFinal dump:");
+        println!("pointer = {}", ptr);
+        for i in 0..dimension {
+            println!("{:?}", &mem[i*dimension..i*dimension+dimension]);
         }
-        i += 1
-    }
-    if !jump_stack.is_empty() {
-        eprintln!("Error char {}: unmatched ] bracket ", jump_stack.pop().unwrap_or_else(|| 0));
-        std::process::exit(1);
-    }
-
-    let mut mem: [u8; SIZE] = [0; SIZE];
-    let mut ptr: usize = 0;
-    let mut buffer = [0; 1]; //buffer for reading a character in
-    //second pass
-    i = 0;
-    while i < content.len() {
-        match content[i] {
-            b'>' => ptr = (ptr + 1) % SIZE,
-            b'<' => {
-                if ptr == 0 {
-                    ptr = SIZE - 1;
-                } else {
-                    ptr = (ptr - 1) % SIZE;
-                }
-            }
-            b'+' => mem[ptr] = mem[ptr].wrapping_add(1),
-            b'-' => mem[ptr] = mem[ptr].wrapping_sub(1),
-            b',' => {
-                match io::stdin().read_exact(&mut buffer) {
-                    Ok(_) => {
-                        let character = buffer[0];
-                        if character.is_ascii() {
-                            mem[ptr] = character;
-                        } else {
-                            eprintln!("Runtime Error: Non-ASCII character entered.");
-                            std::process::exit(1);
-                        }
-                    }
-                    Err(error) => println!("Error reading input: {}", error),
-                }
-            }
-            b'.' => print!("{}", mem[ptr] as char),
-            b'[' | b']' => {
-                if mem[ptr] != 0 && content[i] == b']' || mem[ptr] == 0 && content[i] == b'[' {
-                    match jumps.get(&i) {
-                        Some(x) => i = *x,
-                        None => {
-                            eprintln!("Internal Error: Jump at {i} not found");
-                            std::process::exit(1);
-                        }
-                    }
-                }
-            }
-            b'\n' | b'\t' | b' ' => (),
-            _ => { //comment
-                match jumps.get(&i) {
-                    Some(x) => i = *x,
-                    None => {
-                        eprintln!("Internal Error: Jump at {i} not found");
-                        std::process::exit(1);
-                    }
-                }
-            }
-        }
-        i += 1;
     }
 }
